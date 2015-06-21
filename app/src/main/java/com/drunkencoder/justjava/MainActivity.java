5a58ddd5793d703c0a5a8a4542c917e8b919e212
Panella <panca.boy@tiscali.it>
2015-06-21 18:18:30 +0200

    JustJava0.1
    Just Crap

---
+++ app/src/main/java/com/drunkencoder/justjava/MainActivity.java
@@ -0,0 +1,67 @@
+package com.drunkencoder.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    int quantity = 0;
+    int price = 5;
+    /**dfgdg
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        System.out.print("grazie");
+    }
+
+    public void increment(View view) {
+        quantity++;
+        display(quantity);
+        displayPrice(quantity*price);
+    }
+
+    public void decrement(View view) {
+        quantity++;
+        display(quantity);
+        displayPrice(quantity*price);
+
+    }
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView;
+        quantityTextView = (TextView) findViewById(
+                R.id.textView2);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.textView4);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
+
+
+
+}
